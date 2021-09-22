be00d5d2804e8a6ae7c71ea72e669f99045eb5d4
Alex Li <github@alexv525.com>
2021-09-22 20:41:12 +0800

    Rewrite kotlin part

--- android/src/main/kotlin/com/flutter/moum/screenshot_callback/ScreenshotCallbackPlugin.kt
+++ android/src/main/kotlin/com/flutter/moum/screenshot_callback/ScreenshotCallbackPlugin.kt
@@ -1,43 +1,66 @@
 package com.flutter.moum.screenshot_callback
-import android.os.*
+
+import android.os.Build
+import android.os.FileObserver
+import android.os.Handler
+import android.os.Looper
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import java.io.File
+import java.util.ArrayList
 
-class ScreenshotCallbackPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
-
+class ScreenshotCallbackPlugin : FlutterPlugin, MethodCallHandler {
     private var handler: Handler? = null
     private var fileObserver: FileObserver? = null
-    lateinit var channel: MethodChannel
-    override fun onMethodCall(call: MethodCall, result: Result) {
-        when {
-            call.method.equals("initialize") -> {
+    private var channel: MethodChannel? = null
+
+    override fun onAttachedToEngine(binding: FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, "flutter.moum/screenshot_callback")
+        channel!!.setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+        channel = null
+        if (fileObserver != null) {
+            fileObserver!!.stopWatching()
+        }
+        fileObserver = null
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "initialize" -> {
                 handler = Handler(Looper.getMainLooper())
-                if (Build.VERSION.SDK_INT >= 29) {
-                    //Log.d(TAG, "android x");
-                    val files = mutableListOf<File>()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    val files: MutableList<File> = ArrayList()
                     for (path in Path.values()) {
                         files.add(File(path.path))
                     }
                     fileObserver = object : FileObserver(files, CREATE) {
                         override fun onEvent(event: Int, path: String?) {
-                            //Log.d(TAG, "androidX onEvent");
                             if (event == CREATE) {
-                                handler?.post { channel.invokeMethod("onCallback", null) }
+                                handler!!.post {
+                                    if (channel != null) {
+                                        channel!!.invokeMethod("onCallback", null)
+                                    }
+                                }
                             }
                         }
                     }
                     fileObserver?.startWatching()
                 } else {
-                    //Log.d(TAG, "android others");
                     for (path in Path.values()) {
-                        fileObserver = object : FileObserver(File(path.path), CREATE) {
+                        fileObserver = object : FileObserver(path.path, CREATE) {
                             override fun onEvent(event: Int, path: String?) {
-                                //Log.d(TAG, "android others onEvent");
                                 if (event == CREATE) {
-                                    handler?.post { channel.invokeMethod("onCallback", null) }
+                                    handler!!.post {
+                                        if (channel != null) {
+                                            channel!!.invokeMethod("onCallback", null)
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -46,27 +69,16 @@ class ScreenshotCallbackPlugin : FlutterPlugin, MethodChannel.MethodCallHandler 
                 }
                 result.success("initialize")
             }
-            call.method.equals("dispose") -> {
-                fileObserver?.stopWatching()
+            "dispose" -> {
+                handler = null
+                if (fileObserver != null) {
+                    fileObserver!!.stopWatching()
+                }
                 result.success("dispose")
             }
             else -> {
                 result.notImplemented()
             }
         }
-    }
-
-    companion object {
-        private const val channelName = "flutter.moum/screenshot_callback"
-    }
-
-    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, channelName)
-        channel.setMethodCallHandler(this)
-
-    }
-
-    override fun onDetachedFromEngine(p0: FlutterPlugin.FlutterPluginBinding) {
-
     }
 }
